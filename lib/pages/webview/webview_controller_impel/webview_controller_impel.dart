a3a2d940b7f5398628d09dca8b03ed70e96214a3
KotaHv <92137267+KotaHv@users.noreply.github.com>
2025-08-17 22:30:59 +0800

    fix: improve iframe parsing with origin and sandbox checks

--- lib/pages/webview/webview_controller_impel/webview_controller_impel.dart
+++ lib/pages/webview/webview_controller_impel/webview_controller_impel.dart
@@ -158,13 +158,27 @@ class WebviewItemControllerImpel
     await webviewController!.runJavaScript('''
       var iframes = document.getElementsByTagName('iframe');
       JSBridgeDebug.postMessage('The number of iframe tags is' + iframes.length);
+
+      function isSameOrigin(url) {
+        try {
+          var urlObj = new URL(url, window.location.href);
+          return urlObj.hostname === window.location.hostname;
+        } catch (e) {
+          return false;
+        }
+      }
+
       for (var i = 0; i < iframes.length; i++) {
           var iframe = iframes[i];
           var src = iframe.getAttribute('src');
-          if (src) {
+          var sandbox = iframe.getAttribute('sandbox');
+          if (src && (sandbox || !isSameOrigin(src))) {
             JSBridgeDebug.postMessage(src);
           }
-
+      }
+      for (var i = 0; i < iframes.length; i++) {
+          var iframe = iframes[i];
+          var src = iframe.getAttribute('src');
           if (src && src.trim() !== '' && (src.startsWith('http') || src.startsWith('//')) && !src.includes('googleads') && !src.includes('adtrafficquality') && !src.includes('googlesyndication.com') && !src.includes('google.com') && !src.includes('prestrain.html') && !src.includes('prestrain%2Ehtml')) {
               IframeRedirectBridge.postMessage(src);
               break; 
